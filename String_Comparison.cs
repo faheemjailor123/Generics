97a3b6ed9a06de577f45df8ac20327a2ec2f95c9
faheemjailor123 <93036039+faheemjailor123@users.noreply.github.com>
2021-11-15 23:58:46 +0530

    comparison of three strings

---
+++ String_Comparison.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GenereicsProblem01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calling the generic class method
+            Console.WriteLine(GenericClass.Maxstring("apple","mango","banana"));
+        }
+    }
+}
