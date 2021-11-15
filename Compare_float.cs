ba2222eecee2acb56047a0d207365f4a2eefcec9
faheemjailor123 <93036039+faheemjailor123@users.noreply.github.com>
2021-11-15 23:56:58 +0530

    comparing float between three

---
+++ Compare_float.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GenereicsProblem01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calling the generic class method
+            Console.WriteLine(GenericClass.MaxNumber(100.256, 30.256, 130.0232));
+        }
+    }
+}
