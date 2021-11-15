c4e4dd37bd3dc810002258ff7e90335c355de20e
faheemjailor123 <93036039+faheemjailor123@users.noreply.github.com>
2021-11-15 23:55:00 +0530

    3_array

---
+++ Array(3).cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GenereicsProblem01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GenericClass.MaxNumber(100, 30, 30));
+        }
+    }
+}
